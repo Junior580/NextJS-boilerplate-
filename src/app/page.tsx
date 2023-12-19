bd5327cb439cb8bd402cb5c2280da6f0b4c0241f
Junior da Silva Pereira <junior_silvapereira@hotmail.com>
2023-12-19 14:49:14 +1100

    input component pt1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,6 @@
+import Button from '@/components/Button'
+import Input from '@/components/Input'
+
 export default function Home() {
   return (
     <main className='bg-slate-400 h-screen flex items-stretch'>
@@ -5,19 +8,11 @@ export default function Home() {
         <form className='bg-white flex flex-col justify-center items-center gap-2 p-2 rounded-lg w-80'>
           <h1>Faça seu logon</h1>
 
-          <input
-            placeholder='E-mail'
-            className='bg-red-100 w-full rounded-lg text-slate-950 border-solid placeholder-slate-800 p-2'
-          />
+          <Input placeholder='E-mail' />
 
-          <input
-            placeholder='Password'
-            className='bg-red-100 w-full rounded-lg text-slate-950 border-solid placeholder-slate-800 p-2'
-          />
+          <Input type='password' placeholder='Senha' />
 
-          <button className='bg-orange-500 h-14 rounded-lg w-full py-4 text-slate-700 hover:bg-orange-400 transition duration-300'>
-            Entrar
-          </button>
+          <Button />
         </form>
       </div>
     </main>
---
+++ src/components/Button.tsx
@@ -0,0 +1,7 @@
+export default function Button() {
+  return (
+    <button className='bg-orange-500 h-14 rounded-lg w-full py-4 text-slate-700 font-medium hover:bg-orange-400 transition duration-300'>
+      Entrar
+    </button>
+  )
+}
